285471f5bc9c6ed930524afc43a2fa5c64b94862
Philip Da Silva <philip.dasilva@rangle.io>
2017-07-07 09:08:39 -0400

    AoT Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, InjectionToken } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoggerToken, logger } from './logger.token';
+import { LoggerToken, logger, Logger, loggerFactory } from './logger.token';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,8 @@ import { LoggerToken, logger } from './logger.token';
   ],
   providers: [
     { provide: LoggerToken, useValue: logger }
+    // { provide: LoggerToken, useFactory: loggerFactory }
+    // { provide: LoggerToken, useValue: Logger }
   ],
   bootstrap: [AppComponent]
 })
