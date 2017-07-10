3a734401e1a9d78a1d4f1833c876b7baf31b046b
David Barreto <barretollano@gmail.com>
2017-07-10 11:15:23 -0400

    Added named functions for AoT

--- src/app/logger.token.ts
+++ src/app/logger.token.ts
@@ -1,13 +1,14 @@
 import { InjectionToken } from '@angular/core';
 import { ILogger} from './interfaces';
 
-export const logger = {
-  error() {
-    console.log('error');
-  },
-  warning() {
-    console.warn('warning');
-  }
-};
+export function error() {
+  console.log('error');
+}
+
+export function warning() {
+  console.warn('warning');
+}
+
+export const logger = { error, warning };
 
 export const LoggerToken = new InjectionToken<ILogger>('app.logger');
